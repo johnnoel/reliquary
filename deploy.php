87ce14d5e286b0b15c5ca2814d77f02a6229898e
John Noel <chaostangent@gmail.com>
2021-04-06 16:49:17 +0100

    Tweak how options are passed to deployer

--- deploy.php
+++ deploy.php
@@ -2,9 +2,12 @@
 
 namespace Deployer;
 
+use Symfony\Component\Console\Input\InputOption;
+
 require 'recipe/symfony.php';
 
-// Config
+option('artifact-url', null, InputOption::VALUE_REQUIRED, 'The URL to get build artifacts from');
+option('circleci-token', null, InputOption::VALUE_REQUIRED, 'The personal Circle CI API token to use');
 
 set('application', 'reliquary');
 set('deploy_path', '~/{{application}}');
@@ -14,10 +17,35 @@ add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
-// Hosts
-
 host('67.207.69.0')
     ->setRemoteUser('johnnoel-uk')
 ;
+
+task('deploy:update_code', function () {
+    $artifactUrl = input()->getOption('artifact-url');
+    $circleCiToken = input()->getOption('circleci-token');
+
+    if (!is_string($artifactUrl) || !is_string($circleCiToken)) {
+        throw new \InvalidArgumentException('Either artifact-url or circleci-token not provided');
+    }
+
+    $header = escapeshellarg('Circle-Token: ' . $circleCiToken);
+    $url = escapeshellarg($artifactUrl);
+
+    $jsonRaw = run('curl -sH ' . $header . ' ' . $url);
+    $json = json_decode($jsonRaw, true);
+
+    if (!is_array($json) || count($json) === 0) {
+        throw new \InvalidArgumentException('Invalid JSON returned: ' . $jsonRaw);
+    }
+
+    $url = escapeshellarg($json[0]['url'] . '?' . http_build_query([
+            'circle-token' => $circleCiToken,
+        ]));
+
+    run('wget -qO reliquary.tar.bz2 ' . $url);
+    run('tar xjf reliquary.tar.bz2 -C {{release_path}}');
+});
+task('deploy:vendors', function () { });
 
 after('deploy:failed', 'deploy:unlock');
